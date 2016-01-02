82a5c63cfa8ed49da9a7aa150aafec3c07631271
Travis Johnston <tjohnst1@gmail.com>
2016-01-01 22:30:04 -0800

    add profile component

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,15 +1,34 @@
 var React = require('react');
 var Router = require('react-router');
-var ReactDOM = require('react-dom');
+var Repos = require('./Github/Repos');
+var UserProfile = require('./Github/UserProfile');
+var Notes = require('./Notes/Notes');
 
-var Profile = React.createClass(
-
+var Profile = React.createClass({
+  getInitialState: function(){
+    return {
+      notes: [],
+      bios: {},
+      repos: []
+    }
+  },
   render: function(){
+    console.log(this.props);
     return (
-
+      <div className="row">
+        <div className="col-md-4">
+          <UserProfile username={this.props.params.username} bio={this.state.bio} />
+        </div>
+        <div className="col-md-4">
+          <Repos repos={this.state.repos} />
+        </div>
+        <div className="col-md-4">
+          <Notes notes={this.state.notes} />
+        </div>
+      </div>
     )
   }
 
-);
+});
 
 module.exports = Profile;
