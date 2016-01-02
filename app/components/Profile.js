19e9b8dfbe8c16e6cb681469e0c33cb28a1419b2
Travis Johnston <tjohnst1@gmail.com>
2016-01-01 21:43:12 -0800

    setup homepage routing/display

---
+++ app/components/Profile.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var Router = require('react-router');
+var ReactDOM = require('react-dom');
+
+var Profile = React.createClass(
+
+  render: function(){
+    return (
+
+    )
+  }
+
+);
+
+module.exports = Profile;
--- public/bundle.js
+++ public/bundle.js
@@ -24113,28 +24113,47 @@
 	var React = __webpack_require__(1);
 	var Main = __webpack_require__(209);
 	var Home = __webpack_require__(210);
+	var Profile = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"../components/Profile\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
 	var Router = __webpack_require__(159);
 	var Route = Router.Route;
 	var IndexRoute = Router.IndexRoute;
 
-	module.exports = React.createElement(Route, { path: '/', component: Main });
+	module.exports = React.createElement(
+	  Route,
+	  { path: '/', component: Main },
+	  React.createElement(Route, { path: 'profile/:username', component: Profile }),
+	  React.createElement(IndexRoute, { component: Home })
+	);
 
 /***/ },
 /* 209 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 
 	var React = __webpack_require__(1);
 
 	var Main = React.createClass({
-	  displayName: 'Main',
+	  displayName: "Main",
 
 	  render: function render() {
 	    return React.createElement(
-	      'div',
-	      null,
-	      'Hello World'
+	      "div",
+	      { className: "main-container" },
+	      React.createElement(
+	        "nav",
+	        { className: "navbar navbar-default", role: "navigation" },
+	        React.createElement(
+	          "div",
+	          { className: "col-sm-7 col-sm-offset-2", style: { marginTop: 15 } },
+	          "MENU"
+	        )
+	      ),
+	      React.createElement(
+	        "div",
+	        { className: "container" },
+	        this.props.children
+	      )
 	    );
 	  }
 	});
