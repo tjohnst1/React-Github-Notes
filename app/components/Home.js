34d450075b937fa8bf3e0586c144d42d2aa0e0b8
Travis Johnston <tjohnst1@gmail.com>
2016-01-01 19:19:22 -0800

    initial commit

---
+++ app/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class Home extends React.Component{
+
+  render(){
+    return (
+      <h2 className="text-center">Search by Username Above</h2>
+    )
+  }
+
+
+
+}
